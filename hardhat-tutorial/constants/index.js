65513ec05df6f29b0f813d9f3ada1f354329fc62
crypte-1 <renukavutukuri1@gmail.com>
2022-06-02 18:11:54 +0530

    added index.js

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x1d513C18a07905500AA848779b568c849ce062B9";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
